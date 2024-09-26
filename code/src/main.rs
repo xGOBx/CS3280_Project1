e6e603bbee36b1be7e75069c420f7f6f9c3ec86d
Gaven Pearl <127455079+xGOBx@users.noreply.github.com>
2024-09-26 17:13:50 -0400

    Main Function update
    	The main function now implements both
    	data generation and file sorting

--- code/src/main.rs
+++ code/src/main.rs
@@ -10,6 +10,29 @@ fn main() {
         println!("Branch folders not found. Generating branch folders and sales data...");
     }
 
+    let branch_codes = generate_data::generate_branch_data();
 
+    let output_folder = "data/weekly_summary";
+    if !Path::new(output_folder).exists() {
+        fs::create_dir(output_folder).expect("Failed to create weekly_summary folder");
+    }
 
+    let start = Instant::now();
+
+    let branch_folders = branch_codes
+        .iter()
+        .map(|code| format!("data/{}", code))
+        .collect::<Vec<String>>();
+    
+    let result = lib::process_input_file(&branch_folders);
+
+    match result {
+        Ok(msg) => println!("{}", msg),
+        Err(e) => println!("An error occurred: {:?}", e),
+    }
+
+    let duration = start.elapsed();
+    println!("Total time: {:?}", duration);
+
+    println!("Phew! I am done.");
 }
